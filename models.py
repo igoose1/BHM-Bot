9f04418ac41f6201fb4019d707dd1a84ab669b6d
Oskar Sharipov <oskar.sharipov [at] tuta.io>
2019-08-20 00:04:14 +0300

    Add Post, Admin models and create_tables function

---
+++ models.py
@@ -0,0 +1,28 @@
+from peewee import *
+
+db = SqliteDatabase('data.db')
+
+
+class Post(Model):
+	photos = CharField(max_length=1024, null=True)
+	name = CharField(max_length=128)
+	year = IntegerField(null=True)
+	regisseur = CharField(max_length=128, null=True)
+	overview = TextField()
+	conclusion = TextField(null=True)
+
+	class Meta:
+		database = db
+
+
+class Admin(Model):
+	telegram_id = IntegerField()
+
+	class Meta:
+		database = db
+
+
+def create_tables():
+	model_tuple = (Post, Admin)
+	db.create_tables(model_tuple)
+
