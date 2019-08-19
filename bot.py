ff40df16d391c310d736cbc2c50af8cd392707b4
Oskar Sharipov <oskar.sharipov [at] tuta.io>
2019-08-20 01:30:07 +0300

    Add bot.py file describing whole logic of the bot

---
+++ bot.py
@@ -0,0 +1,48 @@
+import message_process as mp
+import filters
+
+from telegram.ext import *
+
+
+# reading bot's token
+TOKEN = None
+with open('token', 'r') as f:
+	TOKEN = f.read().strip()
+
+# initialize updater
+updater = Update(token=TOKEN)
+dispatcher = updater.dispatcher
+
+# describing commands
+handlers = (
+	CommandHandler(
+		'start',
+		mp.start
+	),
+	CommandHandler(
+		'new',
+		filters=(filters.IsAdmin & ~filters.IsFilling),
+		mp.new
+	),
+	CommandHandler(
+		'reset',
+		filters=(filters.IsAdmin & filters.IsFilling),
+		mp.reset
+	),
+	CommandHandler(
+		'pass',
+		filters=(filters.filters.IsAdmin & filters.IsFilling & filters.CanBeMissed),
+		mp.miss
+	),
+	MessageHandler(
+		filters.IsAdmin & filters.IsFilling,
+		mp.fill
+	)
+)
+
+for h in handlers:
+	dispatcher.add_handler(h)
+
+# yeah, launching
+updater.start_polling()
+
