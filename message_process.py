a5831042370aef47f2d5f281773884756388b79a
Oskar Sharipov <oskar.sharipov [at] tuta.io>
2019-09-01 20:31:24 +0300

    Write possibility to set fields

--- message_process.py
+++ message_process.py
@@ -15,7 +15,7 @@ def new(bot, update):
     post = Post(
         Post.author.telegram_id == update.message.from_chat.id
     )
-    post.author.state = 'conf' + order[0]
+    post.author.state = 'conf'
     post.save()
     fill(bot, update)
 
@@ -24,13 +24,14 @@ def fill(bot, update):
     post = Post.get(
         Post.author.telegram_id == update.message.from_chat.id
     )
-    now_ind = order.index(post.author.state[len('conf'):])
+    now_ind = -1
+    if post.author.state != 'conf':
+        now_ind = order.index(post.author.state[len('conf'):])
+        setattr(post, order[now_ind], update.message.text)
     next_ind = now_ind + 1
     post.author.state = None if next_ind >= len(order) else 'conf' + {order[next_ind]}
 
     if post.author.state is not None:
-        attr = getattr(post, order[now_ind])
-        attr = update.message.text
         update.message.reply(
             'Fill {field_name} field'.format(
                 field_name=order[next_ind]
