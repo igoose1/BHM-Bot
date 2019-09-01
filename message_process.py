a40c5c62ac21a116360bf8894755401f746adef6
Oskar Sharipov <oskar.sharipov [at] tuta.io>
2019-09-01 20:45:39 +0300

    Add miss function and move asking next field to special function

--- message_process.py
+++ message_process.py
@@ -11,9 +11,38 @@ order = (
 )
 
 
+def ask_next(update, now_ind, post):
+    next_ind = now_ind + 1
+    post.author.state = None if next_ind >= len(order) else 'conf' + {order[next_ind]}
+
+    if post.author.state is not None:
+        update.message.reply(
+            'Fill {field_name} field'.format(
+                field_name=order[next_ind]
+            )
+        )
+    else:
+        update.message.reply(
+            'Success!'
+        )
+
+    return post
+
+
+
 def start(bot, update):
     with open('start.txt', 'r') as f:
         update.message.reply(f.read)
+
+
+def miss(bot, update):
+    post = Post.get(
+        Post.author.telegram_id == update.message.from_chat.id
+    )
+    now_ind = order.index(post.author.state[len('conf'):])
+    if getattr(Post, order[now_ind]).null:
+        post = ask_next(update, now_ind, post)
+    post.save()
 
 
 def new(bot, update):
@@ -33,18 +62,5 @@ def fill(bot, update):
     if post.author.state != 'conf':
         now_ind = order.index(post.author.state[len('conf'):])
         setattr(post, order[now_ind], update.message.text)
-    next_ind = now_ind + 1
-    post.author.state = None if next_ind >= len(order) else 'conf' + {order[next_ind]}
-
-    if post.author.state is not None:
-        update.message.reply(
-            'Fill {field_name} field'.format(
-                field_name=order[next_ind]
-            )
-        )
-    else:
-        update.message.reply(
-            'Success!'
-        )
-
+    post = ask_next(update, now_ind, post)
     post.save()
